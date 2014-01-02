9a9c53869a216a1d43c0aeaa124356440bc52288
Jordan Dukart <jordan@discoverygarden.ca>
2014-01-02 19:23:22 +0000

    Don't render the fieldset if there are no fields.

--- templates/islandora-solr-metadata-display.tpl.php
+++ templates/islandora-solr-metadata-display.tpl.php
@@ -14,20 +14,23 @@
  * @see template_preprocess_islandora_solr_metadata_display()
  */
 ?>
-<fieldset <?php $print ? print('class="islandora islandora-metadata"') : print('class="islandora islandora-metadata collapsible collapsed"');?>>
-  <legend><span class="fieldset-legend"><?php print t('Details'); ?></span></legend>
-  <div class="fieldset-wrapper">
-    <dl xmlns:dcterms="http://purl.org/dc/terms/" class="islandora-inline-metadata islandora-metadata-fields">
-      <?php $row_field = 0; ?>
-      <?php foreach($solr_fields as $value): ?>
-        <dt class="<?php print $row_field == 0 ? ' first' : ''; ?>">
-          <?php print $value['display_label']; ?>
-        </dt>
-        <dd class="<?php print $row_field == 0 ? ' first' : ''; ?>">
-          <?php print implode('<br/>', $value['value']); ?>
-        </dd>
-        <?php $row_field++; ?>
-      <?php endforeach; ?>
-    </dl>
-  </div>
-</fieldset>
+
+<?php if (count($solr_fields) > 1): ?>
+  <fieldset <?php $print ? print 'class="islandora islandora-metadata"' : print 'class="islandora islandora-metadata collapsible collapsed"';?>>
+    <legend><span class="fieldset-legend"><?php print t('Details'); ?></span></legend>
+    <div class="fieldset-wrapper">
+      <dl xmlns:dcterms="http://purl.org/dc/terms/" class="islandora-inline-metadata islandora-metadata-fields">
+        <?php $row_field = 0; ?>
+        <?php foreach($solr_fields as $value): ?>
+          <dt class="<?php print $row_field == 0 ? ' first' : ''; ?>">
+            <?php print $value['display_label']; ?>
+          </dt>
+          <dd class="<?php print $row_field == 0 ? ' first' : ''; ?>">
+            <?php print implode('<br/>', $value['value']); ?>
+          </dd>
+          <?php $row_field++; ?>
+        <?php endforeach; ?>
+      </dl>
+    </div>
+  </fieldset>
+<?php endif; ?>
